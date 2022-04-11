52fddba8ced6d1392c2b6d0306d332514fcd09f9
Varuna Jayasiri <vpjayasiri@gmail.com>
2022-04-11 16:58:55 +0530

    links to samples

--- src/neox/__init__.py
+++ src/neox/__init__.py
@@ -14,5 +14,11 @@ This is a simple implementation of GPT-NeoX for inference and fine-tuning.
 * [Checkpoint downloading and loading helpers](checkpoint.html)
 * [Simple dataset for fine-tuning](data.html)
 * [Utilities](utils/index.html)
+
+### [Samples](samples/__init__.py)
+
+* [Generating text with pipeline-parallel](samples/generating_pipe.html)
+* [Generating text by running the model layer-by-layer](samples/generating_single_gpu.html)
+* [Fine tuning the biases with pipeline-parallel](samples/fine_tune_biases.html)
 """
 
